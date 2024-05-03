6b7480c19aa6792d35b8360388ec9a1dfb457296
Gabriel Solem <117821378+GekkoJr@users.noreply.github.com>
2024-05-03 11:56:47 +0200

    Adding email auth (#1)
    * added gitignore
    
    * added gitignore
    
    * added env example
    
    * updated gitignore
    
    * Added sending of verification codes
    
    * Finished auth system

---
+++ api/auth/VerifyCode.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$code = $_POST["code"];
+$email = $_SESSION["email"];
+
+if($code == null) {
+    Header("Location: /Verify.html");
+}
+
+require "../DB.php";
+$result = $conn->query("SELECT * FROM users WHERE email = '$email'")->fetch_assoc();
+if(time() - $result["token_timestamp"] > 300) {
+    echo "koden har utløpt";
+    die();
+}
+
+if ($code != $result["token"]) {
+    echo "koden er feil";
+    die();
+}
+
+$_SESSION["id"] = $result["id"];
+if ($result["role"] == "admin") {
+    $_SESSION["admin"] = true;
+} else {
+    $_SESSION["admin"] = false;
+}
+header("Location: /order.php");
+
+
+
