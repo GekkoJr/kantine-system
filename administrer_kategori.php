61748115c58854244a69fc259d7955e8a8430c05
henrikheh <henrik.hoias@gmail.com>
2024-05-08 13:05:30 +0200

    hbg

--- administrer_kategori.php
+++ administrer_kategori.php
@@ -9,7 +9,7 @@ require_once 'api/DB.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Administrer kategorier</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
