875c912d8a84c9bb13112d0b0041b5addeca95c3
henrikheh <henrik.hoias@gmail.com>
2024-05-08 13:16:51 +0200

    henter kategorier

--- administrer_kategori.php
+++ administrer_kategori.php
@@ -94,7 +94,7 @@ $result = $conn->query($query);
                                     echo '</th>';
                                     echo '<td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap flex items-center justify-end">';
                                     echo '<div class="flex items-center space-x-4">';
-                                    echo '<button type="button" data-modal-target="deleteModal" data-modal-toggle="deleteModal" data-category-id="' . $row['id']; . '" class="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">';
+                                    echo '<button type="button" data-modal-target="deleteModal" data-modal-toggle="deleteModal" class="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">';
                                     echo '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 -ml-0.5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">';
                                     echo '<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />';
                                     echo '</svg>';
