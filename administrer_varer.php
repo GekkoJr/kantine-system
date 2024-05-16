3ab4ad503b06411e7d8a67904a935d5867c8b6c1
henrikheh <henrik.hoias@gmail.com>
2024-05-16 15:44:40 +0200

    fef

--- administrer_varer.php
+++ administrer_varer.php
@@ -165,6 +165,10 @@ $categories = $db->query($query);
                         <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="29" required="">
                     </div>
                     <div class="col-span-2 sm:col-span-1">
+                        <label for="allergi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Allergi</label>
+                        <input type="text" name="allergi" id="allergi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nøtter, gluten..." required="">
+                    </div>
+                    <div class="col-span-2">
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
                         <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <?php
