fd487e5b41ab1e65968e60e57f29aaf4f66ff01c
GekkoJr <gekkojr@proton.me>
2024-05-22 10:39:45 +0200

    small fixes

--- api/order/new.php
+++ api/order/new.php
@@ -33,7 +33,7 @@ if ($order != []) {
 
     $time = time();
     $bruker = $_SESSION["id"];
-    $avdeling = 0;
+    $avdeling = 1;
     $conn->query("insert into bestillinger (bruker_id, time, total_pris, avdeling) VALUES ('$bruker', $time, $totalpris, $avdeling)");
 
     $orderid = $conn->query("SELECT * FROM bestillinger WHERE time = $time")->fetch_assoc()["id"];
